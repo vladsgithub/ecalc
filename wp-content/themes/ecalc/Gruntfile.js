6b4331d489dd645df57645d1c5352857b564521e
Vlad <st_vlad@tut.by>
2017-05-30 09:41:04 +0300

    Grunt adjustments

---
+++ wp-content/themes/ecalc/Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		watch: {
+			files: "less/*.less",
+			tasks: ["less"]
+		},
+		less: {
+			development: {
+				files: {
+					'assets/css/app.css': 'less/app.less'
+				}
+			}
+		},
+		concat: {
+			dist: {
+				src: [
+					'node_modules/angular/angular.js',
+					'node_modules/angular-ui-router/release/angular-ui-router.js',
+					'node_modules/angular-resource/angular-resource.js',
+					'js/*.js',
+					'js/*/*.js'
+				],
+				dest: 'assets/js/production.js'
+			}
+		},
+		uglify: {
+			build: {
+				src: 'assets/js/production.js',
+				dest: 'assets/js/production.min.js'
+			}
+		}
+		//imagemin: {
+		//	dynamic: {
+		//		files: [{
+		//			expand: true,
+		//			cwd: 'ui/pic/',
+		//			src: ['*.{png,jpg,gif}'],
+		//			dest: 'images/build/'
+		//		}]
+		//	}
+		//}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	//grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+	grunt.registerTask('default', ['less', 'concat', 'watch', 'uglify']); // 'watch', 'uglify'
+
+};
