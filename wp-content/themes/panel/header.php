84ce734ea5053e3947f91820d9c803dc9ebf4c9c
Vlad <st_vlad@tut.by>
2018-08-05 20:57:53 +0300

    Start using a new template

--- wp-content/themes/panel/header.php
+++ wp-content/themes/panel/header.php
@@ -66,7 +66,395 @@
 <body id="body" ng-app="app" ng-controller="calculatorCtrl" ng-cloak="true"
 	ng-class="{'open-menu': layout.isOpenMenu, 'open-aside': layout.isOpenAside}" data-upload-status="1">
 
-	<header style="display: none;">
+
+
+
+
+<header>
+
+	<ul class="head flex">
+		<li>
+			<button class="btn solid" title="Меню" ng-click="layout.openMenu()">
+				<i class="fas fa-bars"></i>
+				<b class="no-mobile">Меню</b>
+			</button>
+		</li>
+		<li class="title flex-grow separator">
+			<h1>Cost panel</h1>
+			<h2>Название расчета</h2>
+		</li>
+		<li class="separator">
+			<button class="btn solid" disabled title="Все изменения сохранены">
+				<b class="status-line">ВС</b>
+				<i class="fas fa-save no-mobile"></i>
+			</button>
+		</li>
+		<li class="separator">
+			<button class="btn solid" title="Все расчеты" ng-click="layout.openAside()">
+				<i class="fas fa-ellipsis-v"></i>
+				<b class="no-mobile">Расчеты</b>
+			</button>
+		</li>
+	</ul>
+
+</header>
+
+<nav>
+    <ul class="nav-head flex">
+        <li class="photo">
+
+        </li>
+        <li class="flex-grow">
+            <div class="text-field name solid">
+                <b>
+                    <?
+                        echo $current_user->user_firstname.' '.$current_user->user_lastname;
+                    ?>
+                </b>
+            </div>
+        </li>
+        <li>
+            <button class="btn solid no-shadow" title="Меню" ng-click="layout.isOpenMenu = false">
+                <i class="fas fa-window-close"></i>
+            </button>
+        </li>
+    </ul>
+
+    <div id="navBody" class="nav-body">
+
+        <ul class="section open active" data-level="1">
+            <li class="section-title">
+                <button class="btn no-shadow" data-previous title="Меню">
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="text-field title"><b>Главное меню</b></div>
+            </li>
+
+            <li>
+                <button class="btn solid no-shadow" data-next title="Авторизация">
+                    <i class="fas fa-user-circle"></i>
+                    <b>Авторизация</b>
+                </button>
+
+                <ul class="section" data-level="2">
+                    <li class="section-title">
+                        <button class="btn no-shadow" data-previous title="Авторизация">
+                            <i class="fas fa-user-circle"></i>
+                        </button>
+                        <div class="text-field title"><b>Авторизация</b></div>
+                    </li>
+
+                    <li>
+                        <button class="btn solid no-shadow" data-next>
+                            <i class="fas fa-sign-in-alt"></i>
+                            <b>Войти / Выйти</b>
+                        </button>
+
+                        <ul class="section" data-level="3">
+                            <li class="section-title">
+                                <button class="btn no-shadow" data-previous title="Войти / Выйти">
+                                    <i class="fas fa-sign-in-alt"></i>
+                                </button>
+                                <div class="text-field title"><b>Войти / Выйти</b></div>
+                            </li>
+
+                            <li class="section-body">
+                                <div class="section-page">
+                                    <?
+                // В админке в разделе 'страницы', обязательно необходимо вставить (активировать) шорткод на какой-нибудь странице [clean-login]
+                // пусть даже неиспользуемой на сайте (можно просто выделить целую страницу для авторизации - login),
+                // после этого в настройках Clean Login можно будет увидеть вверху, что указанный шорткод уже используется и тогда
+                // этот плагин будет нормально работать
+
+                                        echo do_shortcode('[clean-login]');
+                                    ?>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <button class="btn solid no-shadow" data-next>
+                            <i class="fas fa-user-plus"></i>
+                            <b>Регистрация</b>
+                        </button>
+
+                        <ul class="section" data-level="3">
+                            <li class="section-title">
+                                <button class="btn no-shadow" data-previous title="Регистрация">
+                                    <i class="fas fa-user-plus"></i>
+                                </button>
+                                <div class="text-field title"><b>Регистрация</b></div>
+                            </li>
+
+                            <li class="section-body">
+                                <div class="section-page">
+                                    <?
+                                        echo do_shortcode('[clean-login-register]');
+                                    ?>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <button class="btn solid no-shadow" data-next>
+                            <i class="fas fa-user-edit"></i>
+                            <b>Редактирование</b>
+                        </button>
+
+                        <ul class="section" data-level="3">
+                            <li class="section-title">
+                                <button class="btn no-shadow" data-previous title="Редактирование">
+                                    <i class="fas fa-user-edit"></i>
+                                </button>
+                                <div class="text-field title"><b>Редактирование</b></div>
+                            </li>
+
+                            <li class="section-body">
+                                <div class="section-page">
+                                    <?
+                                        echo do_shortcode('[clean-login-edit]');
+                                    ?>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <button class="btn solid no-shadow" data-next>
+                            <i class="fas fa-user-check"></i>
+                            <b>Восстановление</b>
+                        </button>
+
+                        <ul class="section" data-level="3">
+                            <li class="section-title">
+                                <button class="btn no-shadow" data-previous title="Восстановление">
+                                    <i class="fas fa-user-check"></i>
+                                </button>
+                                <div class="text-field title"><b>Восстановление</b></div>
+                            </li>
+
+                            <li class="section-body">
+                                <div class="section-page">
+                                    <?
+                                        echo do_shortcode('[clean-login-restore]');
+                                    ?>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <button class="btn solid no-shadow" data-next>
+                    <i class="fas fa-database"></i>
+                    <b>Данные</b>
+                </button>
+
+                <ul class="section" data-level="2">
+                    <li class="section-title">
+                        <button class="btn no-shadow" data-previous title="Авторизация">
+                            <i class="fas fa-user-circle"></i>
+                        </button>
+                        <div class="text-field title"><b>Авторизация</b></div>
+                    </li>
+
+                    <li>
+                        <button class="btn solid no-shadow" data-next>
+                            <i class="fas fa-list-alt"></i>
+                            <b>Типы расходов</b>
+                        </button>
+
+                        <ul class="section" data-level="3">
+                            <li class="section-title">
+                                <button class="btn no-shadow" data-previous title="Типы расходов">
+                                    <i class="fas fa-list-alt"></i>
+                                </button>
+                                <div class="text-field title"><b>Типы расходов</b></div>
+                            </li>
+
+                            <li class="section-body">
+                                <div class="section-page">
+
+                                    <ul>
+                                        <li class="nowrap padding5-0" ng-repeat="expensesType in expCalc.settings.expensesTypes track by $index">
+                                            <button class="btn btn-danger" ng-click="removeExpensesType($index)">
+                                                <i class="fa fa-minus"></i>
+                                            </button>
+                                            {{$index + 1}}
+                                            <input type="text" class="form-control inline-block" ng-model="expensesType.name" onchange="uploadData()"/>
+                                        </li>
+
+                                        <li class="nowrap padding5-0">
+                                            <button class="btn btn-primary" ng-click="addNewExpensesType()">
+                                                <i class="fa fa-plus"></i>
+                                            </button>
+                                            Новый тип расходов
+                                        </li>
+                                    </ul>
+
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <button class="btn solid no-shadow" data-next>
+                    <i class="fas fa-database"></i>
+                    <b>Работа с данными</b>
+                </button>
+
+                <ul class="section" data-level="2">
+                    <li class="section-title">
+                        <button class="btn no-shadow" data-previous="" title="Работа с данными">
+                            <i class="fas fa-database"></i>
+                        </button>
+                        <div class="text-field title"><b>Работа с данными</b></div>
+                    </li>
+
+                    <li class="section-body">
+
+                        <ul class="settings-list">
+                            <li>
+                                <ul class="flex s-p1">
+                                    <li class="s-p1">
+                                        <label class="toggle">
+                                            <input type="checkbox">
+                                            <i></i>
+                                        </label>
+                                    </li>
+
+                                    <li class="flex-grow s-p1">
+                                        <div class="text-field line-through">
+                                            <b>Режим удаления данных</b>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </li>
+                </ul>
+            </li>
+        </ul>
+
+    </div>
+</nav>
+
+<aside ng-class="{'edit-mode': layout.isEditAccountsMode}">
+    <ul class="aside-head flex">
+        <li class="s-p2">
+            <label class="toggle">
+                <input type="checkbox" ng-model="layout.isEditAccountsMode">
+                <i></i>
+            </label>
+        </li>
+
+        <li class="flex-grow">
+            <div class="text-field bold">
+                <b>Режим редактирования</b>
+            </div>
+        </li>
+    </ul>
+
+    <div class="aside-body">
+
+        <ul>
+            <li class="account flex active">
+                <div>
+                    <button class="btn solid edit-mode no-shadow">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </div>
+
+                <div class="flex-grow">
+                    <button class="btn solid view-mode">
+                        <b>Название расчета</b>
+                    </button>
+
+                    <label class="text-input edit-mode">
+                        <input type="text" value="Tекст ДублированныйтекстДублированный текст">
+                        <b>Tекст ДублированныйтекстДублированный текст</b>
+                    </label>
+                </div>
+            </li>
+
+            <li class="account flex">
+                <div>
+                    <button class="btn solid edit-mode no-shadow">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </div>
+
+                <div class="flex-grow">
+                    <button class="btn solid view-mode">
+                        <b>Название расчета</b>
+                    </button>
+
+                    <label class="text-input edit-mode">
+                        <input type="text" value="Tекст ДублированныйтекстДублированный текст">
+                        <b>Tекст ДублированныйтекстДублированный текст</b>
+                    </label>
+                </div>
+            </li>
+
+            <li class="account flex">
+                <div>
+                    <button class="btn solid edit-mode no-shadow">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </div>
+
+                <div class="flex-grow">
+                    <button class="btn solid view-mode">
+                        <b>Название расчета</b>
+                    </button>
+
+                    <label class="text-input edit-mode">
+                        <input type="text" value="Tекст ДублированныйтекстДублированный текст">
+                        <b>Tекст ДублированныйтекстДублированный текст</b>
+                    </label>
+                </div>
+            </li>
+
+
+            <li class="account edit-mode flex">
+                <div>
+                    <button class="btn no-shadow">
+                        <i class="fas fa-plus"></i>
+                    </button>
+                </div>
+
+                <div class="flex-grow s-p1">
+                    <div class="text-field solid">
+                        <b>Создать новый расчет</b>
+                    </div>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+</aside>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<input id="dataHiddenAria" type="checkbox" />
+<div head data-hidden-aria>
 
 		<?
 //			echo 'Username: ' . $current_user->user_login . '<br />';
@@ -95,4 +483,4 @@
 
         <input type="file" onchange="loadData(event)" />
 
-	</header>
+</div>
