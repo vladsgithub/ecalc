b73692107e80a8ae6ae8bee62abf24270bfbc769
Vlad <st_vlad@tut.by>
2019-06-30 18:28:50 +0300

    Google Analytics was added

--- wp-content/themes/panel/header.php
+++ wp-content/themes/panel/header.php
@@ -38,6 +38,16 @@
 	
 	<?php wp_head(); // необходимо для работы плагинов и функционала ?>
 
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129466725-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-129466725-1');
+    </script>
+
     <!-- Yandex.Metrika counter -->
     <script type="text/javascript" >
         (function (d, w, c) {
